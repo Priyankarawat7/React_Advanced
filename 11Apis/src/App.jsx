70ae6251e8a19c6c280833de86a71b3b1323d0cc
Priyankarawat7 <rawatpriyanka7696@gmail.com>
2026-08-20 21:06:51 +0530

    add apis project

--- 11Apis/src/App.jsx
+++ 11Apis/src/App.jsx
@@ -1,11 +1,64 @@
 import React from 'react'
+import axios from 'axios'
+import { useState } from 'react';
 
 function App() {
+
+  const[data,setData]=useState([])
+
+  async function getData()
+  {
+ //const response= await fetch('https://jsonplaceholder.typicode.com/todos/1')
+      // .then(response => response.json())
+      // .then(json => console.log(json))
+     // console.log(response);
+      
+    // const data=await response.json()
+    //   console.log(data);
+
+
+//Axios third party library
+
+   const response= await axios.get('https://jsonplaceholder.typicode.com/todos/')
+
+   console.log(response.data);
+
+   setData(response.data)
+   
+
+
+
+
+
+//const response= fetch("https://jsonplaceholder.typicode.com/todos/1")
+      
+    
+  }
   return (
     <div>
+     <button onClick={getData}>Get Data</button>
       
-    </div>
+      <div>
+        {
+
+          data.map(function (e,idx){
+            return(
+               <div key={e.id}>
+              <h3>{e.id}. {e.title}</h3>
+              <p>Completed: {e.completed ? 'Yes' : 'No'}</p>
+            </div>
+
+            )
+            
+          
+            
+
+          })}
+
+      </div>
+    </div>                         
   )
 }
 
 export default App
+   
