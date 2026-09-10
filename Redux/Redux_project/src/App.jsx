2f9bec3f50ac9a4a46fbf1a79f52d1840dc811e5
Priyankarawat7 <rawatpriyanka7696@gmail.com>
2026-09-10 23:03:31 +0530

    add folder and api

--- Redux/Redux_project/src/App.jsx
+++ Redux/Redux_project/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { fetchPhotos } from './api/mediaApi'
 
 const App = () => {
+  function getPhotos(){
+    fetchPhotos()
+  }
   return (
     <div className='h-screen bg-gray-950 text-white'>
-      App
-    </div>
+      <button className='py-2 px-3 bg-white text-black' 
+      onClick={async ()=>{
+       const data= await fetchPhotos('cat');
+       console.log(data.results);
+       
+        
+      }
+      }>get photos</button>
+    </div >
   )
 }
 
