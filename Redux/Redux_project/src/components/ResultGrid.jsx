efc8cec7ebb1ad7d6aae36b381dbfb3717b48e07
Priyankarawat7 <rawatpriyanka7696@gmail.com>
2026-09-12 23:00:55 +0530

    add dispatch id

--- Redux/Redux_project/src/components/ResultGrid.jsx
+++ Redux/Redux_project/src/components/ResultGrid.jsx
@@ -8,31 +8,59 @@ import { data } from 'react-router-dom';
 
 function ResultGrid() {
 
+    const dispatch=useDispatch()
+
    const {query,activeTab, results, loading,error}= useSelector((store)=>store.search)
     
     useEffect(()=>{
 
     const getData=async ()=>{
+
+        let data=[]
         if(!query.trim()) return
     if(activeTab==='photos'){
         let response=await fetchPhotos(query)
-        const data=response.results
-         console.log(data);
-          }
+        const data=response.results.map((item)=>({
+
+            id:item.id,
+            type:'photo',
+            title:item.alt_description,
+            thumbnail:item.urls.small,
+            src:item.urls.full
+
+        }))
+          console.log(data);
+
+    }    
     if(activeTab==='videos'){
         let response=await fetchVideos(query)
-        const data =response.videos
+        const data =response.videos.map((item)=>({
+           id:item.id,
+            type:'video',
+            title:item.user.name || 'video',
+            thumbnail:item.image,
+            src:item.video_files[0].link
+
+
+        }))
          console.log(data);
      }
      if(activeTab==='gif'){
-        let data=await fetchGif(query)
-       
-         console.log(data);
-     }
-     
+        let response=await fetchGif(query)
+         let  data=response.data.map((item)=>({
+            id:item.id,
+            type:'gif',
+            title:item.title,
+            thumbnail:item.images.fixed_width_small.url,
+            src:item.url
 
+        }))
+       
+        console.log(data);
+     }
+
+     dispatch(setResults(data))
    }
-  
     getData()
         
      },[query,activeTab])
